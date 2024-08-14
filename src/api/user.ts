ac2790b8f20cc0fc6383dcdf419763ca03806e01
Malwande-Dzanibe <malwandedza@gmail.com>
2024-08-14 14:59:08 +0200

    auth-with-next-frontend

--- src/api/user.ts
+++ src/api/user.ts
@@ -11,7 +11,7 @@
 // export default router;
 
 import { Router } from "express";
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
 import sendEmails from "../utils/sendEmail";
 
@@ -92,12 +92,80 @@ router.post("/register", async (req, res) => {
 
     res.status(200).json(tokenToEmail);
   } catch (error) {
-    console.log("this is the error below");
-    console.log({ error });
+    // console.log("this is the error below");
+    // console.log({ error });
 
+    // res.status(500).json({
+    //   //@ts-ignore
+    //   message: error.name,
+    // });
+
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      // The .code property can be accessed in a type-safe manner
+      if (error.code === "P2000") {
+        res.status(500).json({
+          message:
+            "The provided value for the column is too long for the column's type. Column: {column_name}",
+        });
+      }
+      if (error.code === "P2001") {
+        res.status(500).json({
+          message:
+            "The record searched for in the where condition ({model_name}.{argument_name} = {argument_value}) does not exist",
+        });
+      }
+      if (error.code === "P2002") {
+        res.status(500).json({
+          message: "Unique constraint failed on the {constraint}",
+        });
+      }
+      if (error.code === "P2003") {
+        res.status(500).json({
+          message: "Foreign key constraint failed on the field: {field_name}",
+        });
+      }
+      if (error.code === "P2004") {
+        res.status(500).json({
+          message: "A constraint failed on the database: {database_error}",
+        });
+      }
+      if (error.code === "P2005") {
+        res.status(500).json({
+          message:
+            "The value {field_value} stored in the database for the field {field_name} is invalid for the field's type",
+        });
+      }
+      if (error.code === "P2006") {
+        res.status(500).json({
+          message:
+            "The provided value {field_value} for {model_name} field {field_name} is not valid",
+        });
+      }
+      if (error.code === "P2007") {
+        res.status(500).json({
+          message: "Data validation error {database_error}",
+        });
+      }
+      if (error.code === "P2008") {
+        res.status(500).json({
+          message:
+            "Failed to parse the query {query_parsing_error} at {query_position}",
+        });
+      }
+      if (error.code === "P2009") {
+        res.status(500).json({
+          message:
+            "Failed to validate the query: {query_validation_error} at {query_position}",
+        });
+      }
+      if (error.code === "P2010") {
+        res.status(500).json({
+          message: "Raw query failed. Code: {code}. Message: {message}",
+        });
+      }
+    }
     res.status(500).json({
-      //@ts-ignore
-      message: error.name,
+      message: "It is a different error",
     });
   }
 });
@@ -170,12 +238,80 @@ router.get("/tweets", async (req, res) => {
 
     res.status(200).json(tweets);
   } catch (error) {
-    console.log("this is the error below");
+    // console.log("this is the error below");
 
-    console.log(error);
+    // console.log(error);
+    // res.status(500).json({
+    //   //@ts-ignore
+    //   message: error.name,
+    // });
+
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      // The .code property can be accessed in a type-safe manner
+      if (error.code === "P2000") {
+        res.status(500).json({
+          message:
+            "The provided value for the column is too long for the column's type. Column: {column_name}",
+        });
+      }
+      if (error.code === "P2001") {
+        res.status(500).json({
+          message:
+            "The record searched for in the where condition ({model_name}.{argument_name} = {argument_value}) does not exist",
+        });
+      }
+      if (error.code === "P2002") {
+        res.status(500).json({
+          message: "Unique constraint failed on the {constraint}",
+        });
+      }
+      if (error.code === "P2003") {
+        res.status(500).json({
+          message: "Foreign key constraint failed on the field: {field_name}",
+        });
+      }
+      if (error.code === "P2004") {
+        res.status(500).json({
+          message: "A constraint failed on the database: {database_error}",
+        });
+      }
+      if (error.code === "P2005") {
+        res.status(500).json({
+          message:
+            "The value {field_value} stored in the database for the field {field_name} is invalid for the field's type",
+        });
+      }
+      if (error.code === "P2006") {
+        res.status(500).json({
+          message:
+            "The provided value {field_value} for {model_name} field {field_name} is not valid",
+        });
+      }
+      if (error.code === "P2007") {
+        res.status(500).json({
+          message: "Data validation error {database_error}",
+        });
+      }
+      if (error.code === "P2008") {
+        res.status(500).json({
+          message:
+            "Failed to parse the query {query_parsing_error} at {query_position}",
+        });
+      }
+      if (error.code === "P2009") {
+        res.status(500).json({
+          message:
+            "Failed to validate the query: {query_validation_error} at {query_position}",
+        });
+      }
+      if (error.code === "P2010") {
+        res.status(500).json({
+          message: "Raw query failed. Code: {code}. Message: {message}",
+        });
+      }
+    }
     res.status(500).json({
-      //@ts-ignore
-      message: error.name,
+      message: "It is a different error",
     });
   }
 });
