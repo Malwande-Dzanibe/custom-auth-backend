f878275faf7fc16e9a3b25844ee3c81c5120d78d
Malwande-Dzanibe <malwandedza@gmail.com>
2024-09-16 15:19:40 +0200

    custom-auth-backend

--- src/utils/nodemailerTransporter.ts
+++ src/utils/nodemailerTransporter.ts
@@ -3,13 +3,12 @@ import nodemailer from "nodemailer";
 const transporterFunction = () => {
   let transporter = nodemailer.createTransport({
     host: "smtp-mail.outlook.com",
-    secure: true,
-    port: 465,
+    secure: false,
+    port: 587,
     auth: {
       user: "malwandedza@outlook.com",
       pass: process.env.PASS,
     },
-    from: "malwandedza@outlook.com",
   });
 
   return transporter;
