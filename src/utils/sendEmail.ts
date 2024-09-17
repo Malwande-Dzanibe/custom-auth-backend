3f6ba5abe3bff05609d749beafabb0083f7ac1ef
Malwande-Dzanibe <malwandedza@gmail.com>
2024-09-17 14:42:58 +0200

    custom-auth-backend

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,7 +1,5 @@
 import nodemailerTransporter from "./nodemailerTransporter";
 
-let message: string;
-
 const sendEmails = (
   user: {
     id: string;
@@ -24,24 +22,13 @@ const sendEmails = (
     expiration: Date;
   }
 ) => {
-  nodemailerTransporter().sendMail(
-    {
-      from: `"Malwande" <malwandedza@outlook.com>`,
-      to: user.email,
-      subject: `Verification code from custom auth demo project`,
-      text: `Your verification code is ${token.emailToken}, this verification code expires in 10 minutes`,
-      html: `<h4 style={{ color: "red", fontSize: "12px", textAlign: "center" }}>Your verification code is ${token.emailToken}</h4> <p style={{ textAlign: "center" }}>This code exprires in 10 minutes</p>`,
-    },
-    (error: any, infor: any) => {
-      if (error) {
-        console.log(error);
-        message = `${error}`;
-      } else {
-        console.log("email was successfully sent");
-        message = "email was successfully sent";
-      }
-    }
-  );
+  nodemailerTransporter().sendMail({
+    from: `"Malwande" <malwandedza@outlook.com>`,
+    to: user.email,
+    subject: `Verification code from custom auth demo project`,
+    text: `Your verification code is ${token.emailToken}, this verification code expires in 10 minutes`,
+    html: `<h4 style={{ color: "red", fontSize: "12px", textAlign: "center" }}>Your verification code is ${token.emailToken}</h4> <p style={{ textAlign: "center" }}>This code exprires in 10 minutes</p>`,
+  });
 };
 
-export { sendEmails, message };
+export { sendEmails };
