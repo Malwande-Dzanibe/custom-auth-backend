f9cd1eeab3b5652fea65377b9b7bb87bced2be44
Malwande-Dzanibe <malwandedza@gmail.com>
2024-10-24 19:37:43 +0200

    custom-auth-backend

--- src/api/user.ts
+++ src/api/user.ts
@@ -133,7 +133,7 @@ router.post("/register", async (req, res) => {
   } catch (error) {
     console.log(error);
     res.status(500).json({
-      message: error,
+      message: `${error}`,
     });
   }
 });
